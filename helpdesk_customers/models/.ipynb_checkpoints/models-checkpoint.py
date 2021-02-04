37495c65a1acb4860097c9f1d46601c85f94cb57
erickabrego <programador2@grupoley.com>
2021-02-04 17:10:31 +0000

    Agrupación de empresas cliente

--- helpdesk_customers/models/.ipynb_checkpoints/models-checkpoint.py
+++ helpdesk_customers/models/.ipynb_checkpoints/models-checkpoint.py
@@ -6,6 +6,6 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'    
     
     # Creamos el campo el cual estara relacionado para obtener los datos deseados
-    x_parent_id = fields.Many2one(string='Related Company', related='partner_id.commercial_partner_id',
+    x_parent_id = fields.Many2one(string='Compañía Relacionada', related='partner_id.commercial_partner_id',
                                   readonly=True, store=True)
     
