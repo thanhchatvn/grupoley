3a5437170f1a6f957466ec26ce5c97c03ef45492
erickabrego <programador2@grupoley.com>
2021-03-27 22:51:28 +0000

    Stock Delivery Report

--- stock_delivery_report/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ stock_delivery_report/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,33 +3,31 @@
     'name': "stock_delivery_report",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Módulo para crear reportes de vales de entrega con multiples ordenes de compra
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Este módulo agrupa las ordenes de compra con un folio con la finalidad de poder agruparlas y poder imprimir un solo vale de entrada dependiendo
+        de dicho folio
+    """,
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'author': "Soporte Grupo Ley",
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'website': "https://todoo.grupoley.com.mx",
 
-    # always loaded
+
+    'category': 'Inventory',
+
+    'version': '14.0.1',
+
+    'depends': ['base','stock','purchase_order_report'],
+
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/stock_picking.xml',
+        'reports/report.xml',
+        'reports/delivery_slip_report.xml',
     ],
-    # only loaded in demonstration mode
+
     'demo': [
-        'demo/demo.xml',
     ],
 }
