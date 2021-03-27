184bab0956f2073be1624aa06c2a01bfc0164ed5
erickabrego <programador2@grupoley.com>
2021-03-27 23:34:11 +0000

    Planning Custom Report

---
+++ planning_custom_report/models/project_task_type.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+class ProjectTaskType(models.Model):
+    _inherit = 'project.task.type'
+
+    x_in_validation = fields.Boolean(string="Validation Stage", store=True, copied=True,
+                                     help="Task in this stage are in validation before its close")
