990fe829bf9a840bc81f3c432f7a76c076cfb06d
erickabrego <programador2@grupoley.com>
2021-03-28 04:36:33 +0000

    Sale Avoid Multiple Pricelist

---
+++ sale_avoid_multiple_pricelist/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Avoid Multiple Pricelist",
+
+    'summary': """
+        Módulo que nos ayuda a evitar traslapes de tarifas en el mismo producto para el mismo cliente.
+    """,
+
+    'description': """
+        Módulo que nos ayuda a evitar traslapes de tarifas en el mismo producto para el mismo cliente y
+        que así se pueda llevar un mejor control de todas las tarifas sin problema alguno.
+    """,
+
+    'author': "Soporte Grupo Ley",
+
+    'website': "todoo.grupoley.com.mx",
+
+    'category': 'Sales',
+
+    'version': '14.0.1',
+
+    'depends': ['base','sale_management','stock'],
+
+    'data': [
+    ],
+
+    'demo': [
+    ],
+}
