4e26234618912f561c700bff816772b7c9aaa850
erickabrego <programador2@grupoley.com>
2021-04-08 15:58:23 +0000

    Stock Valuation

---
+++ stock_valuation_unit_value/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock Valuation Unit Value",
+
+    'summary': """
+        Nos ayuda a mostrar el costo promedio en la valuación de inventario.
+    """,
+
+    'description': """
+        Este módulo modifica la funcionalidad de como se calcula el cabecero de nuestro costo de unidad
+        al momento de la agrupación en la evaluación de nuestro inventario.
+    """,
+
+    'author': "Soporte Grupo Ley",
+
+    'website': "todoo.grupoley.com.mx",
+
+    'category': 'Inventory',
+
+    'version': '14.0.1',
+
+    'depends': ['base','stock','account'],
+
+    'data': [
+    ],
+
+    'demo': [
+    ],
+}
