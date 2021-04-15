a39024161802568dde5a8264dff27c8218608701
erickabrego <programador2@grupoley.com>
2021-04-15 07:02:33 +0000

    Addenda

--- partner_addenda/models/.ipynb_checkpoints/product_supplierinfo-checkpoint.py
+++ partner_addenda/models/.ipynb_checkpoints/product_supplierinfo-checkpoint.py
@@ -10,3 +10,4 @@ class ProductSupplierinfo(models.Model):
                                     help="Name with which our company is identified by the customer.")
     
     x_center = fields.Char(string='Center', store=True, help="Customer Store")
+    x_uom = fields.Char(string='UOM', store=True, help='Unit Of Measurement')
