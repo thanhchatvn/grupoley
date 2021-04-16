5d2342545736c93521b2b3c9880ae661346a75b9
erickabrego <programador2@grupoley.com>
2021-04-16 07:22:47 +0000

    Addenda

--- partner_addenda/models/.ipynb_checkpoints/product_supplierinfo_sale-checkpoint.py
+++ partner_addenda/models/.ipynb_checkpoints/product_supplierinfo_sale-checkpoint.py
@@ -7,14 +7,14 @@ class ProductSupplierinfoSale(models.Model):
     _name = 'product.supplierinfo.sale' 
     _description = 'Relation between product info from customer'
     
-    partner_name = fields.Many2one('res.partner', ondelete='cascade', requered=True)
-    product_name = fields.Many2one('product.template', ondelete='cascade', required=True)     
+    partner_name = fields.Many2one('res.partner', ondelete='cascade', required=True)
+    product_name = fields.Many2one('product.product', ondelete='cascade', required=True)     
     product_code = fields.Char(string='Product code', help='Code that customer know the product')
     uom = fields.Char(string='UOM', help='Unit Of Measurement')
     uom_additional = fields.Char(string='UOM Additional', help='Unit Of Measurement Additional')
     company = fields.Many2one('res.company','Company', default=lambda self: self.env.company)
     
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = 'product.product'
     
     x_product_supplierinfo = fields.One2many('product.supplierinfo.sale', 'product_name')
