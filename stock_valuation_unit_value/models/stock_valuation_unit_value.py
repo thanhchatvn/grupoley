95b7131f7a9b02d1bde6ccafcd4cca37f6066cff
Erick Abrego <programador2@grupoley.com>
2021-04-22 09:11:38 -0600

    Delete stock_valuation_unit_value directory

--- stock_valuation_unit_value/models/stock_valuation_unit_value.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class StockValuationUnitValue(models.Model):
-    _inherit = 'stock.valuation.layer'
-
-    unit_cost = fields.Monetary('Unit Value', readonly=True, group_operator="avg")
