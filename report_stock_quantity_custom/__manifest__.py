c421f02c6f1a63cbc267d0a3ba2b21bcd99ead04
Erick Abrego <programador2@grupoley.com>
2021-04-27 11:59:53 -0600

    Add files via upload

---
+++ report_stock_quantity_custom/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report Stock Quantity By Category",
+
+    'summary': """
+        Este módulo nos permite agregar la agrupación de categoria de producto en el inventario previsto
+    """,
+
+    'description': """
+        Este módulo nos permite agregar la agrupación de categoria de producto en el inventario previsto
+        como tambien su filtro.
+
+    """,
+
+    'author': "Soporte Grupo Ley",
+
+    'website': "todoo.grupoley.com",
+
+    'category': 'Stock',
+
+    'version': '14.0.1',
+
+    'depends': ['base','stock'],
+
+    'data': [
+        'views/report_stock_quantity.xml',
+    ],
+
+    'demo': [
+    ],
+}
