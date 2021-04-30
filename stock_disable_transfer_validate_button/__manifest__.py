eb73f9b136f8f379925b124fee2b23e18c259a89
Erick Abrego <programador2@grupoley.com>
2021-04-30 13:52:27 -0600

    Add files via upload

---
+++ stock_disable_transfer_validate_button/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock Disable Transfer Validate Button",
+
+    'summary': """
+        Módulo que nos permite ocultar el boton de validar para cuando la transferencia es interna
+    """,
+
+    'description': """
+        Módulo que nos permite ocultar el boton de validad en transferencias cuando estas son internas.
+    """,
+
+    'author': "Soporte Grupo Ley",
+
+    'website': "todoo.grupoley.com",
+
+    'category': 'Stock',
+
+    'version': '14.0.1',
+
+    'depends': ['base','stock'],
+
+    'data': [
+        'views/stock_picking.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
