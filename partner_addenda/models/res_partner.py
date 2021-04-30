5d4499ce52a84030cea95c09c496c9fbd903d5cb
Erick Abrego <programador2@grupoley.com>
2021-04-30 11:34:32 -0600

    Update res_partner.py

--- partner_addenda/models/res_partner.py
+++ partner_addenda/models/res_partner.py
@@ -10,3 +10,4 @@ class ResPartner(models.Model):
     x_center = fields.Char(string='Center', store=True, help="Customer Store")
     x_partner_code = fields.One2many('res.partner.supplierinfo', 'name')
     x_product_info = fields.One2many('product.supplierinfo.sale', 'partner_name')
+    x_edi_identification = fields.Char(string="Identificador EDI", store=True, help="Identificador EDI del contacto")
