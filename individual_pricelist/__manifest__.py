5b5863f2fa9433d1d914d498fbff36d2db464482
Erick Abrego <programador2@grupoley.com>
2021-04-30 11:23:22 -0600

    Add files via upload

---
+++ individual_pricelist/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Individual Pricelist",
+
+    'summary': """
+        Nos ayuda a poder colocar la tarifa individualmente por contactos aunque pertenezcan
+        a una empresa relacionada
+    """,
+
+    'description': """
+       Nos ayuda a poder colocar la tarifa individualmente por contactos aunque pertenezcan
+       a una empresa relacionada
+    """,
+
+    'author': "Soporte Grupo Ley",
+
+    'website': "todoo.grupoley.com.mx",
+
+    'category': 'CRM',
+
+    'version': '14.0.1',
+
+    'depends': ['base','contacts','sale'],
+
+    'data': [
+        'views/res_partner.xml',
+    ],
+
+    'demo': [
+    ],
+}
