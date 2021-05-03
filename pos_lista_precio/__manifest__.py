e206090f77586afcf986d38d34298e1375ae00a7
Erick Abrego <programador2@grupoley.com>
2021-05-03 16:54:33 -0600

    Add files via upload

---
+++ pos_lista_precio/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PDV Lista de Precios",
+    'version': '1.0',
+    'depends': [
+        'point_of_sale',
+    ],
+    'author': "DataWorks",
+    'category': 'Custom',
+    'description': "Personalizacion para limitar Lista de precios por cliente en el PDV",
+    'summary': "Personalizacion para limitar Lista de precios por cliente en el PDV",
+
+    'data': [
+        'views/pos_assets_common.xml',
+    ],
+
+    'qweb': [
+
+    ],
+}
