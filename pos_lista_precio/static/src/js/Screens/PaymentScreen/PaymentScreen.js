2c1057d64372874d2b8f4e699640de0f9bf733e7
Eduardo Velaochaga <eduardo_velaochaga@yahoo.com>
2021-05-04 22:04:24 -0500

    [IMP] Personalizaciones POS

---
+++ pos_lista_precio/static/src/js/Screens/PaymentScreen/PaymentScreen.js
@@ -0,0 +1,17 @@
+console.log('pos_lista_precio.PaymentScreen')
+odoo.define('pos_lista_precio.PaymentScreen', function (require) {
+    "use strict";
+
+    const PaymentScreen = require('point_of_sale.PaymentScreen');
+    const Registries = require('point_of_sale.Registries');
+
+    const PosListaPrecioPaymentScreen = (PaymentScreen) => class extends PaymentScreen {
+        async selectClient() {
+
+        }
+    };
+
+    Registries.Component.extend(PaymentScreen, PosListaPrecioPaymentScreen);
+    return PaymentScreen;
+
+});
