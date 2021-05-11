4cdffdd5ce63f92a61c7ea7eedf349971d49e2ff
erickabrego <77135099+erickabrego@users.noreply.github.com>
2021-05-11 11:56:02 -0600

    Importación de pedidos

--- import_sale_order/models/sale_order.py
+++ import_sale_order/models/sale_order.py
@@ -27,3 +27,13 @@ class ImportChartAccount(models.Model):
             res['x_order_reference'] = self.x_order_reference
             res['x_order_reference_date'] = self.x_order_reference_date
         return res
+
+class SaleAdvanced(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    def _prepare_invoice_values(self, order, name, amount, so_line):
+        res = super(SaleAdvanced, self)._prepare_invoice_values(order,name,amount,so_line)
+        if order.x_order_reference and order.x_order_reference_date:
+            res['x_order_reference'] = order.x_order_reference
+            res['x_order_reference_date'] = order.x_order_reference_date
+        return res
