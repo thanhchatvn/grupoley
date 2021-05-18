53a886109d138b4fcbe88f9fc6a93ef79dda64b1
erickabrego <programador2@grupoley.com>
2021-05-18 18:51:46 +0000

    Recepciones

--- stock_delivery_report/reports/.ipynb_checkpoints/delivery_slip_report-checkpoint.py
+++ stock_delivery_report/reports/.ipynb_checkpoints/delivery_slip_report-checkpoint.py
@@ -16,7 +16,6 @@ class DeliverySlipCustomReport(models.Model):
         # Folio que nos ayudara a filtrar nuestros registros
         folio_filter = ""   
         partner_filter = ""
-        effective_date = ""
 
         # Asignamos el valor a nuestro filtro desde nuestro registro seleccionado
         for i, record in enumerate(docs):
@@ -24,12 +23,19 @@ class DeliverySlipCustomReport(models.Model):
             if i == 0:
                 folio_filter = record.x_order_folio
                 partner_filter = record.partner_id.id
-                effective_year = int(record.date_done.year)
-                effective_month = int(record.date_done.month)
-                effective_day = int(record.date_done.day)
-                initial_effective_date = datetime.datetime(effective_year,effective_month,effective_day,0,0)
-                final_effective_date = datetime.datetime(effective_year,effective_month,effective_day,23,59,59)           
-             
+                if record.date_done:
+                    effective_year = int(record.date_done.year)
+                    effective_month = int(record.date_done.month)
+                    effective_day = int(record.date_done.day)
+                    initial_effective_date = datetime.datetime(effective_year,effective_month,effective_day,0,0)
+                    final_effective_date = datetime.datetime(effective_year,effective_month,effective_day,23,59,59)
+                else:
+                    effective_year = int(record.scheduled_date.year)
+                    effective_month = int(record.scheduled_date.month)
+                    effective_day = int(record.scheduled_date.day)
+                    initial_effective_date = datetime.datetime(effective_year,effective_month,effective_day,0,0)
+                    final_effective_date = datetime.datetime(effective_year,effective_month,effective_day,23,59,59)
+                    
                 
             # Si se seleccionó más de un registro
             elif i > 0:
@@ -37,8 +43,11 @@ class DeliverySlipCustomReport(models.Model):
             else:
                 pass
 
-        # Agrupamos los registros por medio del folio
-        records = self.env['stock.picking'].search(['&','&','&',('date_done','>=',initial_effective_date),('date_done','<=',final_effective_date),('partner_id','=',partner_filter),('x_order_folio','=',folio_filter)])
+        # Agrupamos los registros por medio del folio 
+        if record.date_done:
+            records = self.env['stock.picking'].search(['&','&','&',('date_done','>=',initial_effective_date), ('date_done','<=',final_effective_date),('partner_id','=',partner_filter),('x_order_folio','=',folio_filter)])
+        else:
+            records = self.env['stock.picking'].search(['&','&','&',('scheduled_date','>=',initial_effective_date), ('scheduled_date','<=',final_effective_date),('partner_id','=',partner_filter),('x_order_folio','=',folio_filter)])
 
         return {
             'docs':docs,
