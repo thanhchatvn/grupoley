d03916f550dc80aded2d27dcecc1140ea43dd4f1
Erick Abrego <programador2@grupoley.com>
2021-05-24 06:29:46 -0600

    Add files via upload

---
+++ stock_delivery_note_acc/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "stock_delivery_note_acc",
+
+    'summary': """
+        Formatos de vale de entrega con contabilizacion y sin contabilizacion
+    """,
+
+    'description': """
+         Formatos de vale de entrega con contabilizacion y sin contabilizacion
+    """,
+
+    'author': "",
+
+    'website': "https://todoo.grupoley.com.mx",
+
+
+    'category': 'Inventory',
+
+    'version': '1.0.1',
+
+    'depends': ['base','stock','purchase_order_report'],
+    'css':'static/src/css/invoice-style.css',
+
+    'data': [
+        'views/item_web.xml',
+        'reports/delivery_note_report_acc.xml',
+        'reports/delivery_note_report_view.xml',
+        'views/styles_css.xml',
+
+    ],
+
+    'demo': [
+    ],
+}
---
+++ stock_delivery_note_acc/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import stock_delivery_note_acc
