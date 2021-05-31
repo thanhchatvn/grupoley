b5ec6ee74c406a6280c4faabd065afd56630dd6c
Erick Abrego <programador2@grupoley.com>
2021-05-31 12:47:31 -0600

    Add files via upload

---
+++ avoid_multi_analytic_default_rules/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Avoid Create Same Analytic Rules",
+
+    'summary': """
+        Este modulo limita al usuario a no poder duplicar reglas analiticas
+        con la misma configuración
+    """,
+
+    'description': """
+        Limita al usuario para que este no pueda crear reglas analiticas
+        duplicadas.
+    """,
+
+    'author': "Soporte Grupo Ley",
+    
+    'website': "todoo.grupoley.com.mx",
+ 
+    'category': 'Account',
+    
+    'version': '14.0.1',
+
+    'depends': ['base','account_accountant','analytic','analytic_enterprise'],
+
+    'data': [
+    ],
+    
+    'demo': [
+    ],
+}
