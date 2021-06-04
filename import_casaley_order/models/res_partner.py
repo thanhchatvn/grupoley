c8a46fb14d93e98d70adc199fa5fe8fd851048ac
erickabrego <programador2@grupoley.com>
2021-06-04 00:51:54 +0000

    Casa ley

--- import_casaley_order/models/res_partner.py
+++ import_casaley_order/models/res_partner.py
@@ -5,5 +5,4 @@ from odoo import fields, models, api
 class ImportChartAccount(models.Model):
     _inherit = "res.partner"
 
-    x_route = fields.Many2one('route.pam',string='Ruta (ciudad)',store=True)
     x_zone = fields.Many2one('zone.pam',string='Territorio', store=True)
