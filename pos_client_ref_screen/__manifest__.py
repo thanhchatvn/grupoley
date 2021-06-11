b024e7404c5a6a49e671da26f909ddb27da10454
rosselyn <barroyetardv@tida-services.com>
2021-06-10 23:22:41 -0400

    add new field reference in ClientListScreen POS

---
+++ pos_client_ref_screen/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'POS Client List with reference',
+    'version': '1.0.1',
+    'category': 'Sales/Point of Sale',
+    'sequence': 10,
+    'summary': 'Inherit ticket POS ',
+     'author': "DataWorks",
+    'description': "",
+    'depends': ['base','point_of_sale'],
+    'data': [
+         #'views/templates.xml',
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'qweb': [
+        'static/src/xml/ClientLine.xml',
+        'static/src/xml/ClientListScreen.xml',
+
+    ],
+    'website': '',
+}
